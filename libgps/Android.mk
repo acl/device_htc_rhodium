3ad43b508996dccf655fc23ec5382dabb05c568f
ACL <abel.laura@gmail.com>
2011-11-13 16:01:18 -0500

    Upload gps userland code.

---
+++ libgps/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH:= $(call my-dir)
+
+ifneq ($(TARGET_SIMULATOR),true)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := gps.$(TARGET_BOOTLOADER_BOARD_NAME)
+
+LOCAL_CFLAGS           += -DHAVE_GPS_HARDWARE
+
+LOCAL_C_INCLUDES += \
+	$(TARGET_OUT_HEADERS)/librpc
+
+LOCAL_SRC_FILES += \
+	gps.c \
+	gps_msm7k.c \
+	gps-rpc.c
+
+LOCAL_SHARED_LIBRARIES += \
+	libutils \
+	libcutils \
+	librpc
+
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
