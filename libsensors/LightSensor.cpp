63e53767d95a2a837dbb03c6281cc4bf8afa36aa
ACL <abel.laura@gmail.com>
2011-11-17 02:34:24 -0500

    Hack to make sure brighness does not go to max when LS returns -1 when first initialized. This should be fixed in the kernel.

--- libsensors/LightSensor.cpp
+++ libsensors/LightSensor.cpp
@@ -150,8 +150,8 @@ int LightSensor::readEvents(sensors_event_t* data, int count)
 float LightSensor::indexToValue(size_t index) const
 {
     static const float luxValues[10] = {
-            10.0, 160.0, 225.0, 320.0, 640.0,
-            1280.0, 2600.0, 5800.0, 8000.0, 10240.0
+            10.0, 40.0, 90.0, 160.0, 225.0,
+            320.0, 640.0, 1280.0, 2600.0, 10240.0
     };
 
     const size_t maxIndex = sizeof(luxValues)/sizeof(*luxValues) - 1;
