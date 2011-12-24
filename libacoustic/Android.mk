a79e756ef01659a8de8211a9f1b9347d238aa8a2
ACL <abel.laura@gmail.com>
2011-12-24 04:59:57 -0500

    Add JB's libacoustic to fix gsm broken mic.

---
+++ libacoustic/Android.mk
@@ -0,0 +1,18 @@
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),rhodium)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := libacoustic.c
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libhtc_acoustic
+
+LOCAL_SHARED_LIBRARIES := libc libcutils libm liblog libdl
+
+LOCAL_ARM_MODE := arm
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
+
